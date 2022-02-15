e91f8a3604554a6f8a8fd4ade7fbe0ce84b9a7cb
Santiago Ruiz <hiebra@hotmail.com>
2022-02-15 13:23:51 +0100

    Update Hello.java

--- src/main/java/es/aeat/rs/core/Hello.java
+++ src/main/java/es/aeat/rs/core/Hello.java
@@ -2,7 +2,7 @@ package es.aeat.res.core;
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, World! ");
+    System.out.println("Hello, World!");
   }
 }
   
